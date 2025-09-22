144f76121d599871a7c6064c8005ad4c22d19ffd
Ruixi1313 <lin.ruix@northeastern.edu>
2025-09-22 15:58:05 -0700

    change design plan

---
+++ algorithm.py
@@ -0,0 +1,15 @@
+
+# Assignment: Greedy Algorithm (Individual)
+# Problem: Activity Selection
+
+from typing import List, Tuple
+
+def select_activities(activities):
+    return []
+
+def main():
+    #  code here
+    pass
+
+if __name__ == "__main__":
+    main()
