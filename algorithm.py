e17f364051c0602533f29c504f12f7f380bfc941
Ruixi1313 <132958772+Ruixi1313@users.noreply.github.com>
2025-10-27 00:09:44 -0700

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,15 +1,1 @@
 
-# Assignment: Greedy Algorithm (Individual)
-# Problem: Activity Selection
-
-from typing import List, Tuple
-
-def select_activities(activities):
-    return []
-
-def main():
-    #  code here
-    pass
-
-if __name__ == "__main__":
-    main()
